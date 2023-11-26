5ef2431517114dd605c4ace0e23c32e0f4343e07
jeonghyeonee <jlh1218@naver.com>
2023-11-26 17:25:38 +0900

    update Description

--- Mobile Programming/Assignment/HW05/HW05/app/src/main/java/com/example/hw05/ResultFragment.kt
+++ Mobile Programming/Assignment/HW05/HW05/app/src/main/java/com/example/hw05/ResultFragment.kt
@@ -1,18 +1,12 @@
 package com.example.hw05
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.hw05.databinding.FragmentResultBinding
-import android.os.Parcelable
 
-// Import the AddressInfo class
-import com.example.hw05.AddressInfo
-
-// Fragment to display the result based on saved information in SharedPreferences
 class ResultFragment : Fragment() {
 
     // View Binding instance for the fragment's layout
@@ -25,14 +19,16 @@ class ResultFragment : Fragment() {
         binding = FragmentResultBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        // Cast once to avoid multiple casts
+        val dataTransferListener = requireActivity() as? OnDataTransferListener
+
         // Display retrieved information in the corresponding TextViews
-        binding.textViewName.text = (requireActivity() as? OnDataTransferListener)?.getStudentInfo()?.name
-        binding.textViewAge.text = (requireActivity() as? OnDataTransferListener)?.getStudentInfo()?.age
-        binding.textViewCity.text = (requireActivity() as? OnDataTransferListener)?.getAddressInfo()?.city
-        binding.textViewStudentNum.text = (requireActivity() as? OnDataTransferListener)?.getStudentInfo()?.studentNumber
+        binding.textViewName.text = dataTransferListener?.getStudentInfo()?.name
+        binding.textViewAge.text = dataTransferListener?.getStudentInfo()?.age
+        binding.textViewCity.text = dataTransferListener?.getAddressInfo()?.city
+        binding.textViewStudentNum.text = dataTransferListener?.getStudentInfo()?.studentNumber
 
         return view
     }
-
-
 }
+
